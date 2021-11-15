4df08afb1b3c34191bd154ee71173fec8eff9469
Furkan Karabulut <fkarabu@ncsu.edu>
2021-11-15 01:10:35 -0500

    I have corrected the mess it is working but it is needed to be written

--- src/Board.java
+++ src/Board.java
@@ -21,6 +21,16 @@ public class Board {
             throw new IllegalArgumentException("Invalid cols");
         } else if(this.maximumNumberOfShips < 1) {
             throw new IllegalArgumentException("Invalid ships");
+        } else {
+            grid = new Square[this.numberOfRows][this.numberOfColumns];
+            for (int i=0; i< this.numberOfRows;i++) {
+                for (int j=0;j < this.numberOfColumns;j++) {
+                    grid[i][j] = new Square();
+                }
+            }
+
+            ships = new Ship[this.maximumNumberOfShips];
+
         }
     }
 
@@ -43,15 +53,56 @@ public class Board {
         } else if (startRow < 0 || startRow >= this.numberOfRows) {
             throw new IllegalArgumentException("Invalid row");
         } else if (startCol < 0 || startCol >= this.numberOfColumns) {
-            throw new IllegalArgumentException("Invalid length");
+            throw new IllegalArgumentException("Invalid col");
         } else{
-            if (isHorizontal == true && this.ships.length + startCol <= numberOfColumns) {
+            if (this.maximumNumberOfShips == this.numberOfShips)
+                return false;
+
+            if (isHorizontal == true && (length + startCol) <= numberOfColumns) {
+
+                for (int i=0; i<length;i++) {
+                   if ( grid[startRow][startCol + i].getShip() != null)
+                       return false;
+                }
+
+
                 Ship newShip = new Ship(length, isHorizontal, startRow, startCol);
-                this.ships[this.ships.length]=newShip;
+                for (int i=0;i<this.maximumNumberOfShips;i++)
+                {
+                    if (this.ships[i] == null)
+                    {
+                        this.ships[i]=newShip;
+                        this.numberOfShips++;
+                        break;
+                    }
+                }
+
+                //TODO:Custom code that might be correct!
+                for (int i=0; i<length;i++) {
+                    grid[startRow][startCol + i].addShip(newShip);
+                }
                 return true;
-            } else if (isHorizontal == false && this.ships.length + startRow <= numberOfRows) {
+            } else if (isHorizontal == false && (length + startRow) <= numberOfRows) {
+
+                for (int i=0; i<length;i++) {
+                    if ( grid[startRow+i][startCol].getShip() != null)
+                        return false;
+                }
+
                 Ship newShip = new Ship(length, isHorizontal, startRow, startCol);
-                this.ships[this.ships.length]=newShip;
+                for (int i=0;i<this.maximumNumberOfShips;i++)
+                {
+                    if (this.ships[i] == null)
+                    {
+                        this.ships[i]=newShip;
+                        this.numberOfShips++;
+                        break;
+                    }
+                }
+                //TODO:Custom code that might be correct!
+                for (int i=0;i<length;i++){
+                    grid[startRow + i][startCol].addShip(newShip);
+                }
                 return true;
             }
         }
@@ -83,6 +134,7 @@ public class Board {
     }
 
     public Ship[] getShips() {
+
         return this.ships;
     }
 
@@ -108,13 +160,11 @@ public class Board {
         } else if(col < 0 || col >= numberOfColumns) {
             throw new IllegalArgumentException("Invalid col");
         }
-        boolean y = false;
-        if(grid[row][col].hasBeenHit() == false){
-            y = true;
+        if(grid[row][col].hasBeenHit() == true){
+            return true;
         } else{
-            y = false;
+            return false;
         }
-        return y;
     }
 
     public boolean areAllShipsSunk() {
@@ -133,29 +183,14 @@ public class Board {
     @Override
     public String toString() {
         String dash = "";
-//        String[][] myBoard = new String[this.numberOfRows][this.numberOfColumns];
-//        for(int i = 1; i <= myBoard.length; i++) {
-//            for(int j = 0; j < myBoard[0].length; j++) {
-//                System.out.print(dash);
-//            }
-//            System.out.println();
-//        }
-//       for(int i = 0; i < ships.length; i++) {
-//           if(ships[i].isHorizontal() == true) {
-//               for (int j = 0; j < ships[i].getLength(); j++)
-//                   myBoard[ships[i].getStartRow()][ships[i].getStartCol()+i] =
-//                           grid[ships[i].getStartRow()][ships[i].getStartCol()+i].toString();
-//           } else {
-//               for (int j = 0; j < ships[i].getLength(); j++)
-//                   myBoard[ships[i].getStartRow() + i ][ships[i].getStartCol()] =
-//                           grid[ships[i].getStartRow() + i][ships[i].getStartCol()].toString();
-//           }
-//       }
-        for(int i = 1; i <= this.numberOfRows; i++) {
+        for(int i = 0; i < this.numberOfRows; i++) {
             for(int j = 0; j < this.numberOfColumns; j++) {
                 dash = dash + grid[i][j].toString();
+                if (j < this.numberOfColumns-1)
+                    dash = dash + " ";
+                else
+                    dash = dash + "\n";
             }
-            dash = dash + "\n";
         }
 
        return dash;
