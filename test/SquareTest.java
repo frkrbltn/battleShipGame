30aa41216ced4fe7b9f3088f540e619e4ee31f91
Furkan Karabulut <fkarabu@ncsu.edu>
2021-11-15 12:12:50 -0500

    I wrote test cased for SquareTest class and it passed all.

--- test/SquareTest.java
+++ test/SquareTest.java
@@ -4,7 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-//TODO Javadoc
+/**
+ * This test class will test the method in
+ * the Square class.
+ * @author Furkan Karabulut
+ */
 public class SquareTest {
 
     /** Battleship.Square object */
@@ -28,18 +32,24 @@ public class SquareTest {
         assertFalse(square.hasShip());
     }
 
+    /**
+     * Tests addShip with hasShip and getShip methods.
+     */
     @Test
     public void testAddShip() {
         // TODO: Replace the fail() statement with the following:
         // Create a ship and add it to the square. Then test the hasShip() and getShip() methods.
-        fail("No test added");
+        Ship ship = new Ship(3, true, 3, 1);
+        square.addShip(ship);
+        assertTrue(square.hasShip(), "it should be true cause it has a ship");
+        assertTrue(square.getShip().equals(ship), "it needs to return ship");
     }
 
     @Test
     public void testFireAt() {
         // TODO: Replace the fail() statement with the following:
         // Fire at the square. Then test the hasBeenHit() method.
-        fail("No test added");
+        assertFalse(square.hasBeenHit(), "it is been not hit");
     }
 
     /**
@@ -69,7 +79,18 @@ public class SquareTest {
         // Then add the same ship to the other Battleship.Square and test the equals() method  again.
         // Then fire at the square and test the equals() method again.
         // Then fire at the other Battleship.Square and test the equals() method again.
-        fail("No test added");
+        Ship ship2 = new Ship(3, true, 3, 1);
+        Square square2 = new Square();
+
+        square.addShip(ship2);
+        assertFalse(square.equals(square2.equals(ship2)), "They are not equal");
+        square2.addShip(ship2);
+        assertTrue(square.equals(square2), "They are now equal");
+
+        square.fireAt();
+        assertFalse(square.equals(square2), "it should be false");
+        square2.fireAt();
+        assertTrue(square.equals(square2), "Now, it should be true");
     }
 
     /**
@@ -82,12 +103,22 @@ public class SquareTest {
         assertEquals("W", square.toString(), "Test toString() with no ship and has been hit");
     }
 
+    /**
+     * Tests toString() method for Squares with ships
+     */
     @Test
     public void testToStringWithShip() {
         // TODO: Replace the fail() statement with the following:
         // Create a ship and add it to the square. Then test the toString() method.
         // Then fire at the square and test the toString() method again.
-        fail("No test added");
+        Ship ship = new Ship(2, false, 1, 2);
+        square.addShip(ship);
+        assertEquals("2",square.toString(), "the ship is not null and not been fired\n " +
+                                                             "so it needs to give only length");
+        square.fireAt();
+        assertEquals("R", square.toString(), "the ship is not null and its been hit\n" +
+                                                            "Not it should give the Red");
+
     }
 
 
